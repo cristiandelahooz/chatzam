fcac767be0699e340179d7894102b94f2a722c90
cristiandelahooz <cristiandelahooz@gmail.com>
2024-10-30 17:16:10 -0400

    Initial commit

---
+++ src/main/java/org/cidr/chatzam/ChatzamApplication.java
@@ -0,0 +1,13 @@
+package org.cidr.chatzam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatzamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatzamApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/cidr/chatzam/ChatzamApplicationTests.java
@@ -0,0 +1,13 @@
+package org.cidr.chatzam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatzamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
